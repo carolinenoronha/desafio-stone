dc9aa6aa1ceb466dedde4a375999412169186be4
Caroline <carolnte@gmail.com>
2021-01-22 00:25:04 -0300

    Arquivo JS com a função comentada

---
+++ desafio.js
@@ -0,0 +1,50 @@
+//Os testes foram rodados via Node.js, por isso o console.log para printar as funções na tela no arquivo testes.txt.
+
+function calculaEdistribui(listaCompras,listaEmails){
+
+    let valores = Object.values(listaCompras);
+    let tamanhoListaEmails = listaEmails.length;
+
+    //Esta condição engloba casos em que nenhuma lista testada é vazia
+    if (valores.length !=0 && tamanhoListaEmails !=0){
+
+    //Faz o produto das quantidades de itens por seus preços
+    let produtosPrecoQtd = [];
+    for(i in valores){
+        produtosPrecoQtd.push((valores[i].qtd)*(valores[i].preco))
+    } 
+
+    //Soma os valores referentes aos produtos das quantidades de itens por seus preços
+    let valorTotalCompra = produtosPrecoQtd.reduce((n1,n2)=> n1+n2)
+
+
+    let valoresDistribuidos = []
+
+    //Condicional para distribuir o montante da compra por endereço de email caso seja igual pra todos
+       if (valorTotalCompra%tamanhoListaEmails == 0){
+            for (let i = 0;i<tamanhoListaEmails; i++){
+                valoresDistribuidos.push(valorTotalCompra/tamanhoListaEmails)
+            }
+
+        return valoresDistribuidos
+        } 
+    //Condicional para distribuir o montante da compra por endereço de email caso seja não igual pra todos
+        if (valorTotalCompra%tamanhoListaEmails != 0){
+            for (let i = 0;i<tamanhoListaEmails; i++){
+                valoresDistribuidos.push(Math.floor(valorTotalCompra/tamanhoListaEmails)) 
+            }
+
+            for (let j=0;j < valorTotalCompra%tamanhoListaEmails; j++){
+                valoresDistribuidos[j] = valoresDistribuidos[j]+1
+            }           
+
+               return valoresDistribuidos
+            }
+    }
+
+    //Retorna mensagem de "erro" caso haja uma lista vazia
+
+    else{
+        return "A lista de compras ou a lista de emails está vazia. Por favor, preencha-as devidamente."
+    }
+}
